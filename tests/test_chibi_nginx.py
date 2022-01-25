b8026c38dc2c12969937f836077bd0aefc795a90
dem4ply <dem4ply@gmail.com>
2022-01-25 07:18:58 -0600

    se agrego chibi_nginx para manejar los archivos de nginx

--- tests/test_chibi_nginx.py
+++ tests/test_chibi_nginx.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 import unittest
 from chibi_nginx.nginx import parse, to_string
+from unittest import TestCase
+from chibi.file.temp import Chibi_temp_path
+from chibi_nginx import Chibi_nginx
 
 
 nginx_content = """
@@ -81,7 +84,6 @@ nginx_expected = {
 }
 
 
-
 class Test_chibi_nginx( unittest.TestCase ):
     def setUp(self):
         pass
@@ -97,3 +99,49 @@ class Test_chibi_nginx( unittest.TestCase ):
         data = parse( nginx_content )
         result = to_string( data )
         self.assertEqual( result, nginx_to_string_expected )
+
+
+class Test_chibi_nginx_file( TestCase ):
+    def setUp( self ):
+        self.folder = Chibi_temp_path()
+        self.file_service = self.folder.temp_file( extension='conf' )
+        with open( self.file_service, 'w' ) as f:
+            f.write( nginx_content )
+
+        self.expected = {
+            'events': {
+                'accept_mutex': 'off', 'worker_connections': '10000' },
+            'http': {
+                'access_log': '/var/log/nginx/access.log upstream_time',
+                'default_type': 'json',
+                'error_log': '/var/log/nginx/error.log',
+                'include': [
+                    '/etc/nginx/conf.d/*.conf',
+                    'mime.types',
+                    '/etc/nginx/sites_enabled/*'],
+                'keepalive_timeout': '65',
+                'sendfile': 'off',
+                'server': {
+                    'listen': '80 default_server', 'return': '444'
+                }
+            },
+            'worker_processes': '2',
+            'worker_rlimit_nofile': '30000'
+        }
+
+    def test_should_be_a_dict( self ):
+        service = Chibi_nginx( self.file_service )
+        result = service.read()
+        self.assertIsInstance( result, dict )
+
+    def test_should_be_the_expected( self ):
+        service = Chibi_nginx( self.file_service )
+        result = service.read()
+        self.assertEqual( result, self.expected )
+
+    def test_write_should_work( self ):
+        service = Chibi_nginx( self.file_service )
+        result = service.read()
+        service.write( result )
+        result_after_save = service.read()
+        self.assertEqual( result, result_after_save )
